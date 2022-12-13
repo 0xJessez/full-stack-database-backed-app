480d5476bcb52246d32be2a1cd6a2178805937f8
0xJessez <jesse.zhang919@gmail.com>
2022-12-14 09:58:23 +1100

    adjusted file structure

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE anime_db;
+\c anime_db
+
+CREATE TABLE anime_reviews(
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  image_url TEXT
+
+
+
+
+
+
+
+  
+);
--- main.rb
+++ main.rb
@@ -1,5 +1,18 @@
-     
+require 'dotenv/load'
 require 'sinatra'
+require 'bcrypt'
+require './db/db'
+require 'httparty'
+
+# session
+
+# models
+
+# controllers
+
+# helpers
+
+
 
 get '/' do
   erb :index

ba07eefbe596bfc4b01d6d0322ea4893757dd2ff
0xJessez <jesse.zhang919@gmail.com>
2022-12-14 18:16:39 +1100

    added personal profile view

--- helpers/animes_helper.rb
+++ helpers/animes_helper.rb
@@ -1,4 +1,9 @@
 def review_user(id)
     user = run_sql('SELECT * FROM users WHERE id = $1', [id])[0]
-    user['first_name']
+    user['username']
+end
+
+def anime_details(anime_id, detail)
+    anime = run_sql('SELECT * FROM anime_db WHERE id = $1', [anime_id])[0]
+    anime[detail]
 end
